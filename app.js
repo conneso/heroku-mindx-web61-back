a870840c1bc8efd2eea6b41d51d0f0abefa585be
Hồ Trí Dũng <tridung.max@gmail.com>
2022-10-01 21:13:14 +0700

    change port process.env.PORT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var tutorialRouter = require('./routes/tutorials')
-const port = 3001;
+const port = process.env.PORT || 3001;
 var app = express();
 
 app.use(logger("dev"));
